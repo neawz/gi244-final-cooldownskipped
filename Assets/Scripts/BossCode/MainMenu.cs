222e25c4594c5045cb60639802dee64132b98bc9
Sakonwat <sakonwat.yimnapoe@gmail.com>
2026-04-29 21:50:34 +0700

    UI Main test

---
+++ Assets/Scripts/BossCode/MainMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void play()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+
+
+}
+

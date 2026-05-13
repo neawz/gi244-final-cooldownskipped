838ae1a26fa91aaca15b72867f0e6bbc6880ef29
FaMaLyZ <immerth5@gmail.com>
2026-05-13 23:23:53 +0700

    immy merge but i get broken code
    so im fix by copy new's code from git hub to fix it

--- Assets/Scripts/Debuff.cs
+++ Assets/Scripts/Debuff.cs
@@ -6,16 +6,7 @@ public class Debuff : PowerUp
     public float debuffDuration = 5f;
     public override void EnablePowerUp(PlayerStatus player, PlayerStatus opponent)
     {
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<Collider>().enabled = false;
-        StartCoroutine(ApplyReverseDebuff(opponent));
-    }
-
-    private IEnumerator ApplyReverseDebuff(PlayerController player)
-    {
-        player.ReverseControls();
-        yield return new WaitForSeconds(debuffDuration);
-        player.ReverseControls();
+        opponent.ApplyDebuff(debuffDuration);
         PowerUpObjectPool.GetInstance().ReturnObject(gameObject);
     }
 }
